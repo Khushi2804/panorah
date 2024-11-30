df88a40f367d9a68d147b36e500c8a073187a1bc
Khushi <khushikd28@gmail.com>
2024-11-30 15:11:49 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,astro}", // Adjust the paths to match your project structure
+  ],
+  theme: {
+    // extend: {
+    //   backgroundImage: {
+    //   'footer-bg': "url('/image/Grouppp.svg')", // Use the relative path to your image
+    // },},
+  },
+  plugins: [],
+  
+};
